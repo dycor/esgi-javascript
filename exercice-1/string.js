19d21a5a416be1188b71181b3d71ec3f16866f5d
Correia Lopes Dylan <dylan.correia@hotmail.com>
2018-11-06 16:33:56 +0100

    test

---
+++ exercice-1/string.js
@@ -0,0 +1,83 @@
+//ucfirst
+var str = "Hello it's Dylan";
+
+const ucfirst = function(str) {
+  if(typeof (str) !== "string")
+    return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+//capitalize
+const capitalize = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.toLowerCase().replace( /\b./g, function(char){ return char.toUpperCase(); } );
+};
+
+//camelCase
+const camelCase = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.split(" ").map(a => ucfirst(a)).join("");
+};
+
+//snake_case
+const snake_case = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.split(" ").map(a => a.toLowerCase()).join("_");
+};
+
+
+//leet
+const leet = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  const cryp = {
+    a:"4",
+    e:"3",
+    i:"1",
+    o:"0",
+    u:"(_)",
+    y:"7"
+  }
+
+  str = str.toLowerCase();
+  for (property in str) {
+    console.log(property)
+    str.toLowerCase().replace(property,cryp[property]);
+  }
+
+  return str;
+
+};
+
+//prop_access
+const prop_access = function (str){
+  if(typeof (str) !== "string")
+    return "";
+};
+
+//verlan
+const verlan = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.split("").reverse().join("");
+};
+
+//yoda
+const yoda = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.split(" ").reverse().join(" ");
+};
+
+//vig
+const vig = function (str){
+  if(typeof (str) !== "string")
+    return "";
+  return str.split(" ").reverse().join(" ");
+};
+
+
+console.log(leet(str));
