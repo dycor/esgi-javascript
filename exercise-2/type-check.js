f8f84c41e816a1af08d3d60dbe6c26d42dc6cadf
Correia Lopes Dylan <dylan.correia@hotmail.com>
2018-11-27 15:03:23 +0100

    test

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,14 +1,38 @@
 const type_check_v1 = function(val,type) {
-  if(null === val)
-    return true;
+  if(null === val) {
+    if(type === "null")
+      return true;
+    return false;
+  }
 
-  if(type === "array")
-    return Array.isArray(val)
+  if(type === "array") {
+      return Array.isArray(val);
+  }
 
   if(type === "function")
-    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+    return val && {}.toString.call(val) === '[object Function]';
+
+  if(type === "object")
+    if(Array.isArray(val))
+      return false;
 
   return typeof (val) === type;
 };
 
-// console.log(type_check_v1([1,2],"null"))
+const type_check_v2 = function(val,condition) {
+  if(type_check_v1(val,"object")){
+
+  } else {
+    if(condition.type)
+      return type_check_v1(condition.type);
+  }
+  return false;
+};
+
+// console.log(type_check_v2({prop1: 1},{type:"object"}))
+// console.log(type_check_v2("foo",{type:"string",value:"foo"}))
+// console.log(type_check_v2("bar",{type:"string",value:"foo"}))
+// console.log(type_check_v2(3,{enum:["foo","bar",3]}))
+
+// console.log(type_check_v1(null,"object"))
+console.log(type_check_v1([1,2,3],"object"))
