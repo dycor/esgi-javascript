bbcdc6463df194ea8d08e096e48281d0bb705b97
Correia Lopes Dylan <dylan.correia@hotmail.com>
2018-11-27 14:47:11 +0100

    test

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,5 +1,14 @@
 const type_check_v1 = function(val,type) {
+  if(null === val)
+    return true;
+
+  if(type === "array")
+    return Array.isArray(val)
+
+  if(type === "function")
+    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+
   return typeof (val) === type;
 };
 
-console.log(type_check_v1("1","number"))
+// console.log(type_check_v1([1,2],"null"))
