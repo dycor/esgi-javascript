1e96acf5b93737016173064897a49120e4c36927
Correia Lopes Dylan <dylan.correia@hotmail.com>
2018-11-27 15:13:38 +0100

    test

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,16 +1,17 @@
 const type_check_v1 = function(val,type) {
+  if(type ===  "undifined")
+    return val === undefined;
+
   if(null === val) {
-    if(type === "null")
-      return true;
-    return false;
+    return type === "null";
   }
 
   if(type === "array") {
       return Array.isArray(val);
   }
-
-  if(type === "function")
-    return val && {}.toString.call(val) === '[object Function]';
+  //
+  // if(type === "function")
+  //   return val && {}.toString.call(val) === '[object Function]';
 
   if(type === "object")
     if(Array.isArray(val))
@@ -23,16 +24,25 @@ const type_check_v2 = function(val,condition) {
   if(type_check_v1(val,"object")){
 
   } else {
-    if(condition.type)
-      return type_check_v1(condition.type);
-  }
-  return false;
-};
+    var verified = true;
 
+    if(condition.type)
+      verified = verified && type_check_v1(val,condition.type);
+
+    if(condition.value)
+      verified = verified & type_check_v1(val,condition.type);
+
+    // if(condition.type)
+    //   verified = verified & type_check_v1(val,condition.type);
+
+  }
+  return verified;
+};
+console.log(typeof (type_check_v2))
 // console.log(type_check_v2({prop1: 1},{type:"object"}))
 // console.log(type_check_v2("foo",{type:"string",value:"foo"}))
 // console.log(type_check_v2("bar",{type:"string",value:"foo"}))
 // console.log(type_check_v2(3,{enum:["foo","bar",3]}))
 
-// console.log(type_check_v1(null,"object"))
-console.log(type_check_v1([1,2,3],"object"))
+console.log(type_check_v1(type_check_v2,"function"))
+// console.log(type_check_v1([1,2,3],"object"))
